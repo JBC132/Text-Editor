bb8decec18674a5242fb3f89b394ea23fcc94602
이진영 <jakerhee321@gmail.com>
2023-01-04 12:56:05 +0900

    Smileys

--- Text_Editor.py
+++ Text_Editor.py
@@ -1,6 +1,10 @@
 import PySimpleGUI as sg
 
-smileys = []
+smileys = [
+    'happy',[':)','XD',':D','<3'],
+    'sad',[':(','T_T'],
+    'other',[':3']
+]
 
 menu_layout = [
     ['File', ['Open','Save','---','Exit']],
@@ -20,4 +24,7 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+
+    if event == 'Word Count':
+        print('test')
 window.close()
